b4189cdf84ae1552e69a2da5a448769fc7944f2a
Ethan <ethanlee515@gmail.com>
2021-02-14 12:03:19 +0800

    Useless commit #2

--- Class1.cs
+++ Class1.cs
@@ -4,6 +4,8 @@ public class Class1
 {
     static void Main(string[] args)
     {
+        int i = 15; //actually useless work
+
 		int x = 15;
 		Console.WriteLine(x);
         string s2 = "hello world";
