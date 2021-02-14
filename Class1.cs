eb32b276ea14456a44aeb5243a1cb366c0a1b38f
Ethan Lee <ethanlee515@gmail.com>
2021-02-14 12:03:36 +0800

    Add more useful work

--- Class1.cs
+++ Class1.cs
@@ -4,6 +4,9 @@ public class Class1
 {
     static void Main(string[] args)
     {
+		//more useful work
+		int y = 17;
+		Console.WriteLine(y);
 		int x = 15;
 		Console.WriteLine(x);
         string s2 = "hello world";
